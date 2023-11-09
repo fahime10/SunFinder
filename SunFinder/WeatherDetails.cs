73bddfe5b377ff2b62f1f25ea705f7e96e40bc42
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-11-09 23:35:27 +0000

    Added class to store weather details and edited form view

---
+++ SunFinder/WeatherDetails.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunFinder
+{
+    class WeatherDetails
+    {
+        public class coordinates
+        {
+            // from coord.lon
+            double longitude { get; set; }
+            // from coord.lat
+            double latitude { get; set; }
+        }
+
+        public class weather
+        {
+            // from weather.main
+            string curr_weather { get; set; }
+            // from weather.description
+            string description { get; set; }
+            // from weather.icon
+            string image_link { get; set; }
+        }
+
+        public class main_info
+        {
+            // from main.temp
+            double temperature { get; set; }
+            //from main.pressure
+            double pressure { get; set; }
+            // from main.humidity
+            double humidity { get; set; }
+            // from main.feels_like
+            double feels_like { get; set; }
+        }
+
+        public class wind
+        {
+            // from wind.speed
+            double speed { get; set; }
+        }
+
+        public class main
+        {
+            public coordinates coord { get; set; }
+            public List<weather> weather { get; set; }
+            public main_info info { get; set; }
+            public wind wind { get; set; }
+        }
+    }
+
+}
